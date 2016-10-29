74571d574e4854713b93649b121d85cfe6482f45
GovernorX <xmanflips@gmail.com>
2016-10-29 14:14:32 -0400

    Project initiated

---
+++ app/src/main/java/the_darkside/monopoly_assistant/HomePage.java
@@ -0,0 +1,13 @@
+package the_darkside.monopoly_assistant;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_page);
+    }
+}
